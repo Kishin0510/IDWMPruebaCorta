4d215ad77b0ec2dc20ca01e82ff8b15d360174a5
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2024-05-06 15:43:07 -0400

    feat. Creado el modelo Chair

---
+++ Chair.cs
@@ -0,0 +1,14 @@
+namespace pruebaCortaIDWM
+{
+    public class Chair {
+        public int id {get; set;}
+        public string Name {get; set;} = string.Empty;
+        public string Type {get; set;} = string.Empty;
+        public string Color {get; set;} = string.Empty;
+        public int Height {get; set;}
+        public int Wide {get; set;}
+        public int Deep {get; set;}
+        public int Price {get; set;}
+        public int Stock {get; set;}
+    }
+}
